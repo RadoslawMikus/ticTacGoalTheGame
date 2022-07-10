8f6195c5582cdcca33e5ee3b66c3f45e3b77b411
davidsmith931 <radoslaw.mikus@gmail.com>
2022-07-10 23:36:04 +0200

    Added static methods to Player and Game classes

--- scripts/pregame.js
+++ scripts/pregame.js
@@ -1,10 +1,46 @@
+// let numberOfGames;
 const chooseYourFighter = document.querySelector("h3");
 
 class Game {
-  constructor(howManyGames, player1, player2) {
-    this.howManyGames = howManyGames;
+  constructor(player1, player2) {
     this.player1 = player1;
     this.player2 = player2;
+    this.howManyGames = Game.howManyGames();
+  }
+
+  static unlockStart() {
+    let message;
+    const test = (par1, par2, par3, text, unlock) => {
+      if (
+        typeof this.numberOfGames === par1 &&
+        typeof players.player1 === par2 &&
+        typeof players.player2 === par3
+      ) {
+        if (unlock) {
+          startButton.removeAttribute("disabled");
+        }
+        message = text;
+      }
+    };
+    test("number", "object", "object", "START THE GAME!", true);
+    test("undefined", "object", "object", "CHECK NUMBER OF GAMES", false);
+    test("number", "object", "undefined", "CHOOSE YOUR OPPONENT!", false);
+    test("undefined", "object", "undefined", "CHOOSE YOUR OPPONENT!", false);
+    test("number", "undefined", "undefined", "CHOOSE YOUR FIGHTER!", false);
+
+    return message;
+  }
+
+  static howManyGames() {
+    const howManyGames = document.getElementsByName("howManyGames");
+
+    howManyGames.forEach((how) =>
+      how.addEventListener("change", () => {
+        this.numberOfGames = parseInt(how.value);
+        chooseYourFighter.textContent = this.unlockStart();
+      })
+    );
+    return this.numberOfGames;
   }
 
   start() {
@@ -12,88 +48,60 @@ class Game {
   }
 }
 
+let players = { player1: undefined, player2: undefined };
 class Player {
   constructor(name, img) {
     this.name = name;
     this.img = img;
   }
+
+  static chooseYourFighter() {
+    const fighters = document.querySelectorAll(".fighters");
+    const playerNumber = document.querySelectorAll(".playerNumber");
+
+    fighters.forEach((fighter, number) => {
+      fighter.addEventListener("click", (e) => {
+        const chooseFighter = (short, full) => {
+          e.target.classList.add("chosen");
+          playerNumber[number].textContent = short;
+          playerNumber[number].classList.remove("player1", "player2");
+          if (short === "P1") {
+            playerNumber[number].classList.add("player1");
+          } else {
+            playerNumber[number].classList.add("player2");
+          }
+          players[full] = new Player(
+            e.target.getAttribute("name"),
+            `../assets/fighters/${e.target.classList[1]}.jpg`
+          );
+          chooseYourFighter.textContent = Game.unlockStart();
+        };
+        if (players.player1 !== undefined && players.player2 !== undefined) {
+          players.player1 = undefined;
+          players.player2 = undefined;
+          fighters.forEach((fighter) => {
+            fighter.classList.remove("chosen");
+            playerNumber.forEach((tekst) => {
+              tekst.textContent = "";
+            });
+          });
+        }
+        if (players.player1 === undefined) {
+          chooseFighter("P1", "player1");
+        } else {
+          chooseFighter("P2", "player2");
+        }
+      });
+    });
+  }
 }
 
-const unlockStart = () => {
-  let message;
-  const test = (par1, par2, par3, text, unlock) => {
-    if (
-      typeof numberOfGames === par1 &&
-      typeof players.player1 === par2 &&
-      typeof players.player2 === par3
-    ) {
-      if (unlock) {
-        startButton.removeAttribute("disabled");
-      }
-      message = text;
-    }
-  };
-  test("number", "object", "object", "START THE GAME!", true);
-  test("undefined", "object", "object", "CHECK NUMBER OF GAMES", false);
-  test("number", "object", "undefined", "CHOOSE YOUR OPPONENT!", false);
-  test("undefined", "object", "undefined", "CHOOSE YOUR OPPONENT!", false);
-  test("number", "undefined", "undefined", "CHOOSE YOUR FIGHTER!", false);
+Game.howManyGames();
+Player.chooseYourFighter();
 
-  return message;
-};
-
-let numberOfGames;
-
-const howManyGames = document.getElementsByName("howManyGames");
-howManyGames.forEach((how) =>
-  how.addEventListener("change", () => {
-    numberOfGames = parseInt(how.value);
-    chooseYourFighter.textContent = unlockStart();
-  })
-);
-
-let players = { player1: undefined, player2: undefined };
-
-const fighters = document.querySelectorAll(".fighters");
-const teksty = document.querySelectorAll(".tekst");
-fighters.forEach((fighter, number) => {
-  fighter.addEventListener("click", (e) => {
-    const chooseFighter = (short, full) => {
-      e.target.classList.add("chosen");
-      teksty[number].textContent = short;
-      teksty[number].classList.remove("player1", "player2");
-      if (short === "P1") {
-        teksty[number].classList.add("player1");
-      } else {
-        teksty[number].classList.add("player2");
-      }
-      players[full] = new Player(
-        e.target.getAttribute("name"),
-        `../assets/fighters/${e.target.classList[1]}.jpg`
-      );
-      chooseYourFighter.textContent = unlockStart();
-    };
-
-    if (players.player1 !== undefined && players.player2 !== undefined) {
-      players.player1 = undefined;
-      players.player2 = undefined;
-      fighters.forEach((fighter) => {
-        fighter.classList.remove("chosen");
-        teksty.forEach((tekst) => {
-          tekst.textContent = "";
-        });
-      });
-    }
-    if (players.player1 === undefined) {
-      chooseFighter("P1", "player1");
-    } else {
-      chooseFighter("P2", "player2");
-    }
-  });
-});
-
+let newGame;
 const startButton = document.querySelector(".start");
 startButton.addEventListener("click", () => {
-  const newGame = new Game(numberOfGames, players.player1, players.player2);
+  newGame = new Game(players.player1, players.player2);
   console.log(newGame.start());
 });
